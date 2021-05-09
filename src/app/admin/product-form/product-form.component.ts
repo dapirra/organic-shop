bf2ea740c71ae856f90dc1b1f2550a7742f650df
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 15:14:02 -0400

    feat: Products can be added to the database

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from './../../category.service';
+import { ProductService } from './../../product.service';
 
 @Component({
   selector: 'app-product-form',
@@ -9,15 +10,15 @@ import { CategoryService } from './../../category.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService) {
+  constructor(categoryService: CategoryService, private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
   }
 
   ngOnInit(): void {
   }
 
-  save(product) {
-    console.log(product);
+  save(product): void {
+    this.productService.create(product);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  create(product) { // tslint:disable-line: typedef
+    return this.db.list('/products').push(product);
+  }
 }
