2f7c2aa76aa380642df2bc51868ce966a264feb6
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 04:53:22 -0400

    mod: Added code for AdminAuthGuard

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminAuthGuardService {
+export class AdminAuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.user$
+    .pipe(
+      switchMap(user => this.userService.get(user.uid)),
+      map(appUser => appUser.isAdmin)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,8 +52,8 @@ import { UserService } from './user.service';
       { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
 
-      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard] },
-      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard] },
+      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
     ]),
     NgbModule
   ],
