1cddeec8df56bcb31e1246d30499d13c62be7110
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-08 20:54:02 -0400

    init: ProductsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
