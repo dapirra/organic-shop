d95691ca901aa9c13c9151f3a371c38d2d744a44
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 18:19:59 -0400

    fix:AdminProducts: Error when clicking on 'Edit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,13 +61,18 @@ import { UserService } from './user.service';
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
 
       {
-        path: 'admin/products',
-        component: AdminProductsComponent,
+        path: 'admin/products/new',
+        component: ProductFormComponent,
         canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
-        path: 'admin/products/new',
+        path: 'admin/products/:id',
         component: ProductFormComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent,
         canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
