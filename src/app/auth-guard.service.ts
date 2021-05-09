15aaad41da6ac64a3bd49971f4facdf357484587
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 00:05:01 -0400

    feat: auth-guard now blocks certain pages

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,22 @@
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.user$.pipe(map(user => {
+      if (user) return true; // tslint:disable-line: curly
+
+      this.router.navigate(['/login']);
+      return false;
+    }));
+  }
 }
