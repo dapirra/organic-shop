79c2d43d0c5d509d33dc7b8e95479bcf460708d7
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 00:44:50 -0400

    fix: Make sure to unsubscribe from auth.user$

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  private sub: Subscription;
+
   constructor(private auth: AuthService, router: Router) {
-    auth.user$.subscribe(user => {
+    this.sub = auth.user$.subscribe(user => { // tslint:disable-line: deprecation
       if (user) {
         const returnURL = localStorage.getItem('returnURL');
         router.navigateByUrl(returnURL);
       }
     });
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 }
