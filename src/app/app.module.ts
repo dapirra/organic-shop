2245b6c449c30a598e8814c8cdb4d6bb9e976c78
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 15:04:44 -0400

    init: ProductService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ProductService } from './product.service';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { UserService } from './user.service';
@@ -81,6 +82,7 @@ import { UserService } from './user.service';
     AdminAuthGuard,
     UserService,
     CategoryService,
+    ProductService,
   ],
   bootstrap: [AppComponent]
 })
