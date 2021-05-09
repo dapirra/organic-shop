511e3909c1d02b91863e01c76ef04f230296edb5
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-08 23:01:12 -0400

    mod: Display current user's name

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.scss']
 })
 export class BsNavbarComponent {
+  user: firebase.default.User;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => console.log(x));
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
   logout(): void {
