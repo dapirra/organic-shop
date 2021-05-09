36031c740194a137952b96e44e048a3fce6e0b93
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 19:39:03 -0400

    feat:AdminProducts: Products can now be searched

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ProductService } from './../../product.service';
 
 @Component({
@@ -6,14 +7,30 @@ import { ProductService } from './../../product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products); // tslint:disable-line: deprecation
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string): void {
+    console.log(query);
+    this.filteredProducts = query ?
+      this.products.filter(
+        p => p.payload.val().title.toLowerCase().includes(query.toLowerCase())
+      ) :
+      this.products;
   }
 
 }
