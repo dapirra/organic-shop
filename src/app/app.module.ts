6b1f8daae5fec54028dd9946a8d6eb1e8a84a59a
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-09 20:18:49 -0400

    install: angular-4-data-table
    npm install angular-4-data-table --save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DataTableModule } from 'angular-4-data-table';
 import { CustomFormsModule } from 'ng2-validation';
 
 import { environment } from './../environments/environment';
@@ -47,6 +48,7 @@ import { UserService } from './user.service';
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
