938dca5aa3e5c5a3d6028f6e8d8b1f297f8f2cc7
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-10 04:18:51 -0400

    refact: Moved product card code

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
-  selector: 'app-product-card',
+  selector: 'product-card', // tslint:disable-line: component-selector
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
+  @Input('product') product; // tslint:disable-line: no-input-rename
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
