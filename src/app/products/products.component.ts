af81f6f0d981839d1e13286de9ae81e7b020c93d
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-10 03:43:29 -0400

    fix:ProductsComp: Async issue

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { ProductService } from '../product.service';
 import { CategoryService } from './../category.service';
 
@@ -19,20 +20,18 @@ export class ProductsComponent {
     productService: ProductService,
     categoryService: CategoryService) {
 
-    productService.getAll().subscribe(products => { // tslint:disable-line: deprecation
-      this.products = products;
-
-      route.queryParamMap.subscribe(params => { // tslint:disable-line: deprecation
+    productService.getAll().pipe(switchMap(
+      products => {
+        this.products = products;
+        return route.queryParamMap;
+      })).subscribe(params => { // tslint:disable-line: deprecation
         this.category = params.get('category');
 
         this.filteredProducts = this.category ?
           this.products.filter(p => p.payload.val().category === this.category) :
           this.products;
       });
-    });
 
     this.categories$ = categoryService.getAll();
-
   }
-
 }
