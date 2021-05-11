23bf635f1ee474c5c5ca19b74a9a63b516fb127a
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-11 04:46:37 -0400

    feat:checkout: An order can now store a user

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../auth.service';
 import { OrderService } from '../order.service';
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
@@ -12,23 +13,29 @@ import { ShoppingCartService } from './../shopping-cart.service';
 export class CheckOutComponent implements OnInit, OnDestroy {
   shipping: any = {};
   cart: ShoppingCart;
-  subscription: Subscription;
+  userID: string;
+  cardSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
+    private authService: AuthService,
     private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) {}
 
   async ngOnInit(): Promise<void> {
     const cart$ = await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart); // tslint:disable-line: deprecation
+    this.cardSubscription = cart$.subscribe(cart => this.cart = cart); // tslint:disable-line: deprecation
+    this.userSubscription = this.authService.user$.subscribe(user => this.userID = user.uid);  // tslint:disable-line: deprecation
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.cardSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder(): void {
     const order = {
+      userId: this.userID,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.cart.items.map(i => {
