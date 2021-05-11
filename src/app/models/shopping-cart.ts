f6dd8e7f9ba5ce3bc5bcc03b9e7dda09c65fa182
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-10 20:44:48 -0400

    feat: Display all items on shopping cart page

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -3,6 +3,10 @@ import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
   constructor(public items: ShoppingCartItem[]) {}
 
+  get productIDs(): string[] {
+    return Object.keys(this.items);
+  }
+
   get totalItemsCount(): number {
     let count = 0;
     for (const productID in this.items) { // tslint:disable-line: forin
