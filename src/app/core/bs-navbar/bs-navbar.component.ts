927b16ba4f0fcb7c386e030c4c4b60f2e06c17e4
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-20 21:57:24 -0400

    ui:navbar: Created a new category dropdown menu

--- src/app/core/bs-navbar/bs-navbar.component.ts
+++ src/app/core/bs-navbar/bs-navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AppUser } from '@shared/models/app-user';
 import { ShoppingCart } from '@shared/models/shopping-cart';
-import { Observable, Subscription } from 'rxjs';
 import { AuthService } from '@shared/services/auth.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ShoppingCartService } from '@shared/services/shopping-cart.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar', // tslint:disable-line: component-selector
@@ -12,14 +14,24 @@ import { ShoppingCartService } from '@shared/services/shopping-cart.service';
 })
 export class BsNavbarComponent implements OnInit, OnDestroy {
   appUser: AppUser;
+  category: string;
   cart$: Observable<ShoppingCart>;
+  categories$: Observable<unknown[]>;
   private appUserSub: Subscription;
 
-  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { }
+  constructor(
+    private auth: AuthService,
+    private shoppingCartService: ShoppingCartService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    ) {
+    }
 
-  async ngOnInit(): Promise<void> {
-    this.appUserSub = this.auth.appUser$.subscribe(appUser => this.appUser = appUser); // tslint:disable-line: deprecation
-    this.cart$ = (await this.shoppingCartService.getCart());
+    async ngOnInit(): Promise<void> {
+      this.appUserSub = this.auth.appUser$.subscribe(appUser => this.appUser = appUser); // tslint:disable-line: deprecation
+      this.cart$ = (await this.shoppingCartService.getCart());
+      this.categories$ = this.categoryService.getAll();
+      this.route.queryParamMap.subscribe(params => this.category = params.get('category'));
   }
 
   ngOnDestroy(): void {
--- src/app/shopping/components/products/product-filter/product-filter.component.ts
+++ src/app/shopping/components/products/product-filter/product-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CategoryService } from '@shared/services/category.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'product-filter', // tslint:disable-line: component-selector
@@ -7,7 +8,7 @@ import { CategoryService } from '@shared/services/category.service';
   styleUrls: ['./product-filter.component.scss']
 })
 export class ProductFilterComponent {
-  categories$;
+  categories$: Observable<unknown[]>;
   @Input('category') category; // tslint:disable-line: no-input-rename
 
   constructor(categoryService: CategoryService) {
